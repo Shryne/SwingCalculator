4304da110323875fd4b159f82104de93e58f2274
Steven <enomeral@gmx.de>
2018-11-21 21:26:54 +0100

    add interface for wrapped gui components

---
+++ src/main/java/ui/wrapper/WrappedComponent.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Eugen Deutsch
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions: The above
+ * copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ * ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package ui.wrapper;
+
+import java.awt.Component;
+import java.awt.Container;
+
+/**
+ * Defines the wrapper GUI forms.
+ * @since 0.3.0
+ */
+public interface WrappedComponent {
+    /**
+     * Adds this component on the given container. This way I can add my own
+     * wrapped components to other swing components without using getters or
+     * extending the original swing component.
+     * @param container The container to add on. {@link Container} is the
+     *  most abstract class that has the {@link Container#add(Component)}
+     *  method.
+     */
+    void addOn(Container container);
+}
